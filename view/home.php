8eca7a91093c21dc943b6e10adb2c3808e15791c
Karene01 <karenerakotonindrina@gmail.com>
2024-02-07 20:34:37 +0100

    interface

--- view/home.php
+++ view/home.php
@@ -1,10 +1,20 @@
 <?php
 include("view/head.php");
 include("view/navbar.php");
-echo '<h2 style="padding: 16px;">Hello bienvenue sur notre site !</h2>';
+
 foreach($pizza_Moment as $pizza){
-    echo "<h3>Le hot du moment " .$pizza['nom_pizza']. "</h3>";
-    echo $pizza['prix_pizza'];
+    ?>
+    <div id="hot">
+        <?php
+    echo '<h1 id="texte">Le HOT du moment </h1>';
+    echo '<img src="'.$pizza['image_pizza'].'" id="pizza_Moment"/>';
+    echo '<h2 id="prix">'.$pizza['prix_pizza']. "€". '</h2>';
+    echo '<h1 id="nom_pizza"> '.$pizza['nom_pizza']. '</h1>';
+    
+    
+        ?>
+    
+    <?php
 }
 include("view/footer.html");
 
