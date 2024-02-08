0d0d3f5fce662a9bfe16249a810adf1a8c7acc65
Mourchidi MFOUMBY <mfoumbymourchidi@gmail.com>
2024-02-08 07:50:11 +0100

    feat: centrage de l'image

--- view/home.php
+++ view/home.php
@@ -1,7 +1,7 @@
 <?php
 include("view/head.php");
 include("view/navbar.php");
-
+echo '<section class="section-home">';
 foreach($pizza_Moment as $pizza){
     ?>
     <div id="hot">
@@ -10,12 +10,8 @@ foreach($pizza_Moment as $pizza){
     echo '<img src="'.$pizza['image_pizza'].'" id="pizza_Moment"/>';
     echo '<h2 id="prix">'.$pizza['prix_pizza']. "€". '</h2>';
     echo '<h1 id="nom_pizza"> '.$pizza['nom_pizza']. '</h1>';
-    
-    
-        ?>
-    
-    <?php
 }
+echo '</section>';
 include("view/footer.html");
 
 
